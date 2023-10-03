6777c309f00bb8b11e3502f1533658da96511a06
Luis Fabiano <139286649+LuisJuniorPSJ@users.noreply.github.com>
2023-10-03 16:35:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal(".title", { rotate: { x: 0, y: 80, z: 0 }, duration: 1500 });
+sr.reveal(".title", { origin: "right", distance: "80px", duration: 1500 });
 
-sr.reveal(".img_edit", { rotate: { x: 180, y: 360, z: 0 }, duration: 1500 });
+sr.reveal(".img_edit", { origin: "right", distance: "80px", duration: 1500 });
 
-sr.reveal(".my_profile", { rotate: { x: 180, y: 360, z: 0 }, duration: 1500 });
+sr.reveal(".my_profile", { origin: "right", distance: "80px", duration: 1500 });
 
-sr.reveal(".content-text", { rotate: { x: 0, y: 80, z: 0 }, duration: 1500 });
+sr.reveal(".content-text", {
+  origin: "right",
+  distance: "80px",
+  duration: 1500,
+});
 
-sr.reveal(".content-tecs", { rotate: { x: 0, y: 80, z: 0 }, duration: 1500 });
+sr.reveal(".content-tecs", {
+  origin: "right",
+  distance: "80px",
+  duration: 1500,
+});
 
-sr.reveal(".card-content", { rotate: { x: 0, y: 80, z: 0 }, duration: 1500 });
-
-function typeWrite(elemento) {
-  const textoArray = elemento.innerHTML.split("");
-  elemento.innerHTML = " ";
-  textoArray.forEach(function (letra, i) {
-    setTimeout(function () {
-      elemento.innerHTML += letra;
-    }, 65 * i);
-  });
-}
-const titulo = document.querySelector(".title");
-typeWrite(titulo);
+sr.reveal(".card-content", {
+  origin: "right",
+  distance: "80px",
+  duration: 1500,
+});
 
 //Menu Mobile
 const menuIcon = document.getElementById("menu-icon");
